c88b21718c71d6d62ebda24407a2816a10f5fc22
lrramadhanii <lailatulrizqi.lr@gmail.com>
2018-12-04 19:39:29 +0700

    controller - melihat data wali kelas, mengubah data wali kelas, tambah akun wali kelas

--- application/controllers/Controller_waliKelas.php
+++ application/controllers/Controller_waliKelas.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Controller_waliKelas extends CI_Controller {
 
 	public function __construct(){
-		
+
 		parent::__construct();
 		$this->load->model('Model_waliKelas');
 		$this->load->library('session');
@@ -39,6 +39,56 @@ class Controller_waliKelas extends CI_Controller {
 		);
 		$this->load->view('headerAdmin');
 		$this->load->view('AWaliKelasAktif', $param);
+		$this->load->view('footer');
+	}
+	public function ambilDataWaliKelas($id){
+		$data = $this->Model_waliKelas->ambilDataWaliKelas($id);
+		$param = array(
+			'data'		=> $data,
+		);
+
+		$this->load->view('headerAdmin');
+		$this->load->view('ABiodataWaliKelas', $param);
+		$this->load->view('footer');
+	}
+
+	public function ubahDataWaliKelas(){
+		$tabel = 'wali_kelas';
+
+		$id = $this->input->post('id');
+
+		$data = array(
+			'nama'				=> $this->input->post('nama'),
+			'nip'				=> $this->input->post('nip'),
+			'password'			=> md5($this->input->post('password')),
+		);
+
+		$this->Model_waliKelas->ubahDataWaliKelas($id, $data, $tabel);
+
+		$data = $this->Model_waliKelas->tampilWaliKelasAktif();
+		$param = array(
+			'data'		=> $data,
+		);
+		$this->load->view('headerAdmin');
+		$this->load->view('AWaliKelasAktif', $param);
+		$this->load->view('footer');
+	}
+
+	public function tambahAkunWaliKelas(){
+		$tabel = 'wali_kelas';
+		$data = array(
+			'nama'		=> $this->input->post('nama'),
+			'nip'		=> $this->input->post('nip'),
+			'password'	=> md5($this->input->post('psw')),
+			'status'	=> 'aktif'
+		);
+
+		$this->Model_waliKelas->tambahAkunWaliKelas($data, $tabel);
+		$param = array(
+			'buatAkun'		=> "ya",
+		);
+		$this->load->view('headerAdmin');
+		$this->load->view('dashboardAdmin', $param);
 		$this->load->view('footer');
 	}
 }
