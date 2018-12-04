a853d32490c7aff3ee958c0022e90f86d4f1d057
habridio <habridio88@gmail.com>
2018-12-04 19:44:48 +0700

    controller - kelola mapel dan siswa dalam kelas

--- application/controllers/Controller_kelas.php
+++ application/controllers/Controller_kelas.php
@@ -98,3 +98,68 @@ class Controller_kelas extends CI_Controller {
 		$this->load->view('AKelas', $param);
 		$this->load->view('footer');
 	}
+	public function tambahSiswaDalamKelas(){
+		$tabel = 'anggota_kelas';
+		$id_kelas = $this->input->post('id');
+		$kelas = $this->Model_kelas->ambilDataKelas($id_kelas);
+		$siswa = $this->Model_siswa->cekDataSiswa($this->input->post('nis'),$kelas->tahun_ajaran);
+		if ($siswa != false) {
+			$data = array(
+				'id_kelas' 		=> $id_kelas,
+				'id_siswa'		=> $siswa->id_siswa,
+				'tahun_ajaran' 	=> $kelas->tahun_ajaran
+			);
+			$this->Model_kelas->tambahSiswaDalamKelas($siswa->id_siswa, $id_kelas, $kelas->tahun_ajaran);
+			$this->ambilDataKelas($id_kelas,'dataSiswa');
+		} else if ($siswa == false) {
+			$siswa = $this->Model_kelas->ambilDataSiswaDalamKelas($id_kelas);
+			$param = array(
+				'keberadaan'	=> false,
+				'id_kelas' 		=> $id_kelas,
+				'nama_kelas' 	=> $kelas->nama_kelas,
+				'siswa'			=> $siswa,
+				'tahun_ajaran' 	=> $kelas->tahun_ajaran
+			);
+			$this->load->view('headerAdmin');
+			$this->load->view('APerbaruiSiswaDalamKelas', $param);
+			$this->load->view('footer');
+		}
+	}
+
+	public function hapusSiswaDalamKelas($id_siswa,$id_kelas,$tahun_ajaran){
+		$this->Model_kelas->hapusSiswaDalamKelas($id_kelas, $id_siswa, $tahun_ajaran);
+		$this->ambilDataKelas($id_kelas,'dataSiswa');
+	}
+
+	public function tambahMapelDalamKelas(){
+		$tabel = 'detail_mata_pelajaran';
+		$id_kelas = $this->input->post('id');
+		$mapel = $this->Model_mapel->cekDataMapel($this->input->post('mapel'), $id_kelas);
+		if ($mapel != false) {
+			$data = array(
+				'id_kelas' 				=> $id_kelas,
+				'id_mata_pelajaran'		=> $mapel->id_mata_pelajaran
+			);
+			$this->Model_kelas->tambahMapelDalamKelas($data, $tabel);
+			$this->ambilDataKelas($id_kelas,'dataMapel');
+		} else if ($mapel == false) {
+			$mapel = $this->Model_kelas->ambilDataMapelDalamKelas($id_kelas);
+			$kelas = $this->Model_kelas->ambilDataKelas($id_kelas);
+			$param = array(
+				'keberadaan'	=> false,
+				'id_kelas' 		=> $id_kelas,
+				'nama_kelas'	=> $kelas->nama_kelas,
+				'mapel'			=> $mapel,
+			);
+			$this->load->view('headerAdmin');
+			$this->load->view('APerbaruiMapelDalamKelas', $param);
+			$this->load->view('footer');
+		}
+	}
+
+	public function hapusMapelDalamKelas($id_mata_pelajaran,$id_kelas){
+		$tabel = 'detail_mata_pelajaran';
+		$this->Model_kelas->hapusMapelDalamKelas($id_kelas, $id_mata_pelajaran, $tabel);
+		$this->ambilDataKelas($id_kelas,'dataMapel');
+	}
+}
