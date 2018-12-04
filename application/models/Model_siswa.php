de0dca417e669cb0f9de66a1718668e732ba78b9
habridio <habridio88@gmail.com>
2018-12-04 20:13:22 +0700

    model - kelola akun siswa

--- application/models/Model_siswa.php
+++ application/models/Model_siswa.php
@@ -20,3 +20,30 @@ class Model_siswa extends CI_Model{
 		$result = $query->result();
 		return (count($result) > 0) ? $result[0] : false ;
 	}
+	function ubahDataSiswa($id, $data, $tabel){
+		$this->db->where('id_siswa', $id);
+		$this->db->update($tabel, $data);
+	}
+
+	function tambahAkunSiswa($data, $tabel){
+		$this->db->insert($tabel,$data);
+	}
+
+	function cekDataSiswa($nis, $tahun_ajaran){
+		$query = $this->db->query("select * from siswa join anggota_kelas on siswa.id_siswa = anggota_kelas.id_siswa where siswa.nis = '$nis' and anggota_kelas.tahun_ajaran = '$tahun_ajaran' and siswa.status = 'aktif'");
+		$result = $query->result();
+		if (count($result) > 0) {
+			return false;
+		} else if (count($result) == 0) {
+			$query = $this->db->query("select * from siswa where nis = '$nis' and status = 'aktif'");
+			$result = $query->result();
+			return (count($result) > 0) ? $result[0] : false ;
+		}
+	}
+
+	function dataSiswa($idKelas){
+        $query = $this->db->query("select * from siswa join anggota_kelas on siswa.id_siswa = anggota_kelas.id_siswa where anggota_kelas.id_kelas = $idKelas ");
+   		 return $query->result();
+	}
+
+}
