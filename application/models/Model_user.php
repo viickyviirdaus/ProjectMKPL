1fcbd29f2d954e19778e81a026ef2905ef427ad5
habridio <habridio88@gmail.com>
2018-12-04 20:04:35 +0700

    model - tambah akun admin

--- application/models/Model_user.php
+++ application/models/Model_user.php
@@ -4,4 +4,8 @@ class Model_user extends CI_Model{
 	function loginAdmin($table,$where){		
 		return $this->db->get_where($table,$where);
 	}
+
+	function tambahAkunAdmin($data, $tabel){
+		$this->db->insert($tabel,$data);
+	}
 }
