f6a4b99162b4a83cb5f2aa275e95d22da461d885
lrramadhanii <lailatulrizqi.lr@gmail.com>
2018-12-04 19:48:40 +0700

    controller - Mengubah data mapel, menambah mapel

--- application/controllers/Controller_mapel.php
+++ application/controllers/Controller_mapel.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Controller_mapel extends CI_Controller {
 
 	public function __construct(){
-		
+
 		parent::__construct();
 		$this->load->model('Model_mapel');
 		$this->load->library('session');
@@ -29,6 +29,43 @@ class Controller_mapel extends CI_Controller {
 
 		$this->load->view('headerAdmin');
 		$this->load->view('APerbaruiMataPelajaran', $param);
+		$this->load->view('footer');
+	}
+	public function ubahDataMapel(){
+		$tabel = 'mata_pelajaran';
+		$id = $this->input->post('id');
+		$data = array(
+			'nama_mata_pelajaran'	=> $this->input->post('nama_mapel'),
+			'kkm'					=> $this->input->post('kkm'),
+		);
+
+		$this->Model_mapel->ubahDataMapel($id, $data, $tabel);
+
+		$data = $this->Model_mapel->tampilMapel();
+		$param = array(
+			'data' 	=> $data,
+		);
+
+		$this->load->view('headerAdmin');
+		$this->load->view('AMataPelajaran', $param);
+		$this->load->view('footer');
+	}
+
+	public function tambahMapel(){
+		$tabel = 'mata_pelajaran';
+		$data = array(
+			'nama_mata_pelajaran'	=> $this->input->post('nama'),
+			'kkm'					=> $this->input->post('kkm'),
+		);
+		$this->Model_mapel->tambahMapel($data, $tabel);
+
+		$data = $this->Model_mapel->tampilMapel();
+		$param = array(
+			'data' 	=> $data,
+		);
+
+		$this->load->view('headerAdmin');
+		$this->load->view('AMataPelajaran', $param);
 		$this->load->view('footer');
 	}
 
