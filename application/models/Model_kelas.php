d02605b6a7203a0647de43ec73fc1efefe0922ca
Ade Wija Nugraha <adewijanugraha@Ade-Wija-Nugraha.local>
2018-12-04 20:11:19 +0700

    model - tambah kelas, tambah mapel dalam kelas, tambah dan hapus siswa dalam kelas,

--- application/models/Model_kelas.php
+++ application/models/Model_kelas.php
@@ -27,5 +27,35 @@ class Model_kelas extends CI_Model{
 		$this->db->update($tabel, $data);
 	}
 
-	
+	function tambahKelas($data, $tabel){
+		$this->db->insert($tabel,$data);
+	}
+
+	function tambahSiswaDalamKelas($id_siswa, $id_kelas, $tahun_ajaran){
+		$this->db->query("insert into anggota_kelas (id_siswa, id_kelas, tahun_ajaran) values ('$id_siswa', '$id_kelas', '$tahun_ajaran')");
+
+		$query = $this->db->query("select * from mata_pelajaran join detail_mata_pelajaran on mata_pelajaran.id_mata_pelajaran = detail_mata_pelajaran.id_mata_pelajaran join kelas on kelas.id_kelas = detail_mata_pelajaran.id_kelas where kelas.id_kelas = '$id_kelas'");
+
+		$jmlMapel = $query->num_rows();
+
+		for ($i=0; $i < $jmlMapel ; $i++) {
+			$dataRow = $query->row_array($i);
+			$id_mata_pelajaran = $dataRow['id_mata_pelajaran'];
+			$this->db->query("insert into nilai (id_siswa, id_kelas, id_mata_pelajaran, tahun_ajaran) values ('$id_siswa', '$id_kelas', '$id_mata_pelajaran', '$tahun_ajaran')");
+		}
+	}
+
+	function tambahMapelDalamKelas($data, $tabel){
+		$this->db->insert($tabel,$data);
+	}
+
+	function hapusSiswaDalamKelas($id_kelas, $id_siswa, $tahun_ajaran){
+		$this->db->query("delete from anggota_kelas where id_kelas = '$id_kelas' and id_siswa = '$id_siswa'");
+
+		$this->db->query("delete from nilai where id_kelas = '$id_kelas' and id_siswa = '$id_siswa' and tahun_ajaran = '$tahun_ajaran'");
+	}
+
+	function hapusMapelDalamKelas($id_kelas, $id_mata_pelajaran, $tabel){
+		$this->db->query("delete from $tabel where id_kelas = '$id_kelas' and id_mata_pelajaran = '$id_mata_pelajaran'");
+	}
 }
